3de84da76a9cf967bf2ce3c483eb1b3785dd1130
Karl Brocklehurst <kairakau@gmail.com>
2019-01-25 11:53:20 +1300

    update routes for fear, anxiety, doubt, stress, and form

--- routes/stress.js
+++ routes/stress.js
@@ -3,11 +3,11 @@ var express = require('express');
 var router = express.Router()
 
 
-router.get('/', function(req, res){
- 
-    res.send('Stress')
-  })
-  
+router.get('/', function (req, res) {
+
+  res.render('stress')
+})
+
 //   res.status(500).send('DATABASE ERROR: '+ err.message)
 
 
